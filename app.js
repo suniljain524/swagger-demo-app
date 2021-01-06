da596d24de4008c2744d2118b4779a469a3ee22c
Sunil Jain <sunil.jain@convoy.com>
2021-01-06 22:29:56 +0530

    test build again

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-'use strict';
+"use strict";
 
-const devConfig = require('./config/dev_config.json');
-const prodConfig = require('./config/prod_config.json');
-let SwaggerExpress = require('swagger-express-mw');
-let app = require('express')();
-let helmet = require('helmet');
-let _ = require('lodash');
-let env = require('./env');
+const devConfig = require("./config/dev_config.json");
+const prodConfig = require("./config/prod_config.json");
+let SwaggerExpress = require("swagger-express-mw");
+let app = require("express")();
+let helmet = require("helmet");
+let _ = require("lodash");
+let env = require("./env");
 
 module.exports = app; // for testing
 
 app.use(helmet());
 
-let config = app.config = {
-  appRoot: __dirname // required config
-};
+let config = (app.config = {
+  appRoot: __dirname, // required config
+});
 
-app.use('/health-check', require('./routes/health'));
+app.use("/health-check", require("./routes/health"));
 
 SwaggerExpress.create(config, (err, swaggerExpress) => {
-  if (err) { throw err; }
+  if (err) {
+    throw err;
+  }
 
   _.extend(app.config, swaggerExpress.runner.config);
 
-  process.env.SERVER_ENVIRONMENT == 'prod' ? _.extend(app.config, prodConfig) : _.extend(app.config, devConfig);
+  process.env.SERVER_ENVIRONMENT == "prod"
+    ? _.extend(app.config, prodConfig)
+    : _.extend(app.config, devConfig);
 
   app.use(swaggerExpress.runner.swaggerTools.swaggerUi());
-  require('./middlewares/logger').init(app);
-  require('./middlewares/db').init(app);
-  require('./middlewares/cache').init(app);
-  require('./middlewares/swagger').init(app, swaggerExpress, { });
-  require('./middlewares/error').init(app);
+  require("./middlewares/logger").init(app);
+  require("./middlewares/db").init(app);
+  require("./middlewares/cache").init(app);
+  require("./middlewares/swagger").init(app, swaggerExpress, {});
+  require("./middlewares/error").init(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port, () => {
-    console.log('server started on http://127.0.0.1:' + port );
-  })
+    console.log("server started on http://127.0.0.1:" + port);
+  });
 });
